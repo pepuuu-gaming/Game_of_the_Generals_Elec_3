02a1c06c34951c53c5ea6ee451f6f1f32e83d262
markfausty <angelofaustino05@gmail.com>
2020-03-02 02:41:45 +0800

    Tutorial Page 5

--- Game_of_the_Generals/Game of the Generals/TutorialPage5.Designer.vb
+++ Game_of_the_Generals/Game of the Generals/TutorialPage5.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class TutorialPage4
+Partial Class TutorialPage5
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,39 +22,25 @@ Partial Class TutorialPage4
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TutorialPage4))
-        Me.Label1 = New System.Windows.Forms.Label()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TutorialPage5))
         Me.back_to_menu = New System.Windows.Forms.Button()
         Me.BtnTutorialNext = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("OCR A Extended", 50.0!)
-        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(368, 25)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(590, 69)
-        Me.Label1.TabIndex = 4
-        Me.Label1.Text = "BASIC TUTORIAL"
         '
         'back_to_menu
         '
@@ -66,7 +52,7 @@ Partial Class TutorialPage4
         Me.back_to_menu.Location = New System.Drawing.Point(40, 40)
         Me.back_to_menu.Name = "back_to_menu"
         Me.back_to_menu.Size = New System.Drawing.Size(63, 54)
-        Me.back_to_menu.TabIndex = 215
+        Me.back_to_menu.TabIndex = 216
         Me.back_to_menu.UseVisualStyleBackColor = True
         '
         'BtnTutorialNext
@@ -81,7 +67,7 @@ Partial Class TutorialPage4
         Me.BtnTutorialNext.Location = New System.Drawing.Point(1000, 600)
         Me.BtnTutorialNext.Name = "BtnTutorialNext"
         Me.BtnTutorialNext.Size = New System.Drawing.Size(232, 51)
-        Me.BtnTutorialNext.TabIndex = 216
+        Me.BtnTutorialNext.TabIndex = 217
         Me.BtnTutorialNext.Text = "NEXT"
         Me.BtnTutorialNext.UseVisualStyleBackColor = False
         '
@@ -93,8 +79,19 @@ Partial Class TutorialPage4
         Me.Label2.Location = New System.Drawing.Point(116, 170)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(344, 43)
-        Me.Label2.TabIndex = 217
+        Me.Label2.TabIndex = 218
         Me.Label2.Text = "The Soldiers:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("OCR A Extended", 50.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(368, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(590, 69)
+        Me.Label1.TabIndex = 219
+        Me.Label1.Text = "BASIC TUTORIAL"
         '
         'Label3
         '
@@ -104,7 +101,7 @@ Partial Class TutorialPage4
         Me.Label3.Location = New System.Drawing.Point(120, 229)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(832, 66)
-        Me.Label3.TabIndex = 218
+        Me.Label3.TabIndex = 220
         Me.Label3.Text = "  Eliminate any lower ranking officer, the " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  private and the flag."
         '
         'Label4
@@ -114,53 +111,42 @@ Partial Class TutorialPage4
         Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
         Me.Label4.Location = New System.Drawing.Point(160, 313)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(456, 35)
-        Me.Label4.TabIndex = 219
-        Me.Label4.Text = "•  5 star General (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label4.Size = New System.Drawing.Size(288, 35)
+        Me.Label4.TabIndex = 221
+        Me.Label4.Text = "•  Colonel(1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("OCR A Extended", 25.0!)
         Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label5.Location = New System.Drawing.Point(160, 381)
+        Me.Label5.Location = New System.Drawing.Point(160, 382)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(456, 35)
-        Me.Label5.TabIndex = 220
-        Me.Label5.Text = "•  4 star General (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label5.Size = New System.Drawing.Size(393, 35)
+        Me.Label5.TabIndex = 222
+        Me.Label5.Text = "•  LT. Colonel (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("OCR A Extended", 25.0!)
         Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label6.Location = New System.Drawing.Point(160, 447)
+        Me.Label6.Location = New System.Drawing.Point(160, 446)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(456, 35)
-        Me.Label6.TabIndex = 221
-        Me.Label6.Text = "•  3 star General (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label6.Size = New System.Drawing.Size(267, 35)
+        Me.Label6.TabIndex = 223
+        Me.Label6.Text = "•  Major (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("OCR A Extended", 25.0!)
         Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label7.Location = New System.Drawing.Point(160, 512)
+        Me.Label7.Location = New System.Drawing.Point(160, 524)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(456, 35)
-        Me.Label7.TabIndex = 222
-        Me.Label7.Text = "•  2 star General (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Font = New System.Drawing.Font("OCR A Extended", 25.0!)
-        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(56, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label8.Location = New System.Drawing.Point(160, 573)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(456, 35)
-        Me.Label8.TabIndex = 223
-        Me.Label8.Text = "•  1 star General (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label7.Size = New System.Drawing.Size(309, 35)
+        Me.Label7.TabIndex = 224
+        Me.Label7.Text = "•  Captain (1)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'PictureBox1
         '
@@ -169,95 +155,80 @@ Partial Class TutorialPage4
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(42, 37)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 224
+        Me.PictureBox1.TabIndex = 225
         Me.PictureBox1.TabStop = False
         '
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(166, 381)
+        Me.PictureBox2.Location = New System.Drawing.Point(166, 380)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(42, 37)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox2.TabIndex = 225
+        Me.PictureBox2.TabIndex = 226
         Me.PictureBox2.TabStop = False
         '
         'PictureBox3
         '
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(166, 512)
+        Me.PictureBox3.Location = New System.Drawing.Point(166, 446)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(42, 37)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox3.TabIndex = 226
+        Me.PictureBox3.TabIndex = 227
         Me.PictureBox3.TabStop = False
         '
         'PictureBox4
         '
         Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
-        Me.PictureBox4.Location = New System.Drawing.Point(166, 445)
+        Me.PictureBox4.Location = New System.Drawing.Point(166, 522)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(42, 37)
         Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox4.TabIndex = 227
+        Me.PictureBox4.TabIndex = 228
         Me.PictureBox4.TabStop = False
         '
-        'PictureBox5
-        '
-        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
-        Me.PictureBox5.Location = New System.Drawing.Point(166, 573)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(42, 37)
-        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox5.TabIndex = 228
-        Me.PictureBox5.TabStop = False
-        '
-        'TutorialPage4
+        'TutorialPage5
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1264, 681)
-        Me.Controls.Add(Me.PictureBox5)
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.BtnTutorialNext)
         Me.Controls.Add(Me.back_to_menu)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "TutorialPage4"
+        Me.Name = "TutorialPage5"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Game of the Generals"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
     Friend WithEvents back_to_menu As Button
     Friend WithEvents BtnTutorialNext As Button
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents PictureBox4 As PictureBox
-    Friend WithEvents PictureBox5 As PictureBox
 End Class
---
+++ Game_of_the_Generals/Game of the Generals/TutorialPage5.vb
@@ -0,0 +1,16 @@
+﻿Public Class TutorialPage5
+    Private Sub back_to_menu_Click(sender As Object, e As EventArgs) Handles back_to_menu.Click
+        TutorialPage4.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub BtnTutorialNext_Click(sender As Object, e As EventArgs) Handles BtnTutorialNext.Click
+        'TutorialPage6.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub TutorialPage5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As RoundButton = New RoundButton
+        a.Round(BtnTutorialNext, 40)
+    End Sub
+End Class
