aa38a4d9f0db5f8f7ac8fae9cdc58057fabb8a1d
pepuuu-gaming <tgpatrick26@gmail.com>
2020-03-14 21:13:08 +0800

    remove check button

--- Game_of_the_Generals/Game of the Generals/PopupNameInputBox.Designer.vb
+++ Game_of_the_Generals/Game of the Generals/PopupNameInputBox.Designer.vb
@@ -24,7 +24,6 @@ Partial Class PopupNameInputBox
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.setHostName = New System.Windows.Forms.Button()
-        Me.check = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -43,29 +42,18 @@ Partial Class PopupNameInputBox
         'setHostName
         '
         Me.setHostName.Font = New System.Drawing.Font("OCR A Extended", 11.0!)
-        Me.setHostName.Location = New System.Drawing.Point(246, 88)
+        Me.setHostName.Location = New System.Drawing.Point(208, 88)
         Me.setHostName.Name = "setHostName"
         Me.setHostName.Size = New System.Drawing.Size(64, 25)
         Me.setHostName.TabIndex = 1
         Me.setHostName.Text = "SET"
         Me.setHostName.UseVisualStyleBackColor = True
         '
-        'check
-        '
-        Me.check.Font = New System.Drawing.Font("OCR A Extended", 11.0!)
-        Me.check.Location = New System.Drawing.Point(176, 88)
-        Me.check.Name = "check"
-        Me.check.Size = New System.Drawing.Size(64, 25)
-        Me.check.TabIndex = 2
-        Me.check.Text = "CHECK"
-        Me.check.UseVisualStyleBackColor = True
-        '
         'PopupNameInputBox
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 161)
-        Me.Controls.Add(Me.check)
         Me.Controls.Add(Me.setHostName)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "PopupNameInputBox"
@@ -77,5 +65,4 @@ Partial Class PopupNameInputBox
 
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents setHostName As Button
-    Friend WithEvents check As Button
 End Class
